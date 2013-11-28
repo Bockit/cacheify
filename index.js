9213a7af4fb9caaf22277d180b52a1f79fb11f2c
James Hiscock <james@small.mu>
2013-11-29 08:07:25 +1100

    Changed cacheify to optionally take a function that returns a leveldb api compatible instance instead of the instance itself, also added a filter function. Changed the api significantly.

--- index.js
+++ index.js
@@ -2,26 +2,50 @@ var through  = require('through')
   , crypto   = require('crypto')
   , concat   = require('concat-stream')
 
-function cacheify (db, cachee, hash) {
+function cacheify (cachee, _db) {
 
-  if (!hash) {
-    hash = function (content) {
-      return crypto
-        .createHash('md5')
-        .update(content)
-        .digest('hex');
-    }
+  /**
+   * Default to taking the md5 of a file
+   */
+  hash = function (content) {
+    return crypto
+      .createHash('md5')
+      .update(content)
+      .digest('hex');
   }
 
-  return function (file) {
+  /**
+   * Default to caching every file
+   */
+  filter = function() {
+    return true
+  }
+
+  /**
+   * Transforms a file, reading it from cache instead if the hash exists in the
+   * cache already.
+   */
+  transform = function (file) {
+    // Early exit if we don't match the filter
+    if (!filter(file)) {
+      return through()
+    }
+
     var data = ''
 
     return through(write, end)
 
+    // Build up the file data...
     function write (buf) { data += buf; }
+
+    // Do our transform if we need to, or get it from the cache if we can.
     function end () {
       var hashed = hash(data)
       var self = this
+      var db = _db
+
+      // Let db be a function that returns a leveldb api compatible object.
+      if (typeof db === 'function') db = db()
 
       db.get(hashed, function (err, transformed) {
         if (err) {
@@ -44,6 +68,40 @@ function cacheify (db, cachee, hash) {
       });
     }
   }
+
+  /**
+   * Lets you set a custom filter function. A filter function receives one
+   * argument, the name of the file. Return true to check cacheify for a file,
+   * false to use a default through stream.
+   */
+  transform.filter = function(fn) {
+    filter = fn
+
+    return transform
+  }
+
+  /**
+   * Lets you set a custom hash function. The hash function receives two
+   * arguments, the contents of the file and the name of the file. It's up to
+   * you if and how you choose to use them.
+   *
+   * If you pass a RegExp object instead of a function, we'll wrap it in a
+   * function that checks the filename on the regexp, returning true for a
+   * match.
+   */
+  transform.hash = function(_fn) {
+    var fn = _fn
+    if (fn instanceof RegExp) {
+      fn = function(name) {
+        return _fn.test(name)
+      }
+    }
+    hash = fn
+
+    return transform
+  }
+
+  return transform
 }
 
 module.exports = cacheify
